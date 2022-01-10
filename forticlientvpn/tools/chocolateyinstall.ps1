9502c3383f62337f1c7bceeb5ec9320d751f8da5
David Klein <davekl1999@gmail.com>
2022-01-10 09:33:18 -0500

    update links and checksums to new version

--- forticlientvpn/tools/chocolateyinstall.ps1
+++ forticlientvpn/tools/chocolateyinstall.ps1
@@ -1,22 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop'
-$packageName   = 'forticlientvpn'
-$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url           = 'https://d3gpjj9d20n0p3.cloudfront.net/forticlient/downloads/FortiClientVPNSetup_6.2.6.0951.exe'
-$checksum      = 'B85F5E9AECC86FF4FBBA25EA064604B48D3CA723FC4AFA7F5CEF3D289E21D84E'
-$url64         = 'https://d3gpjj9d20n0p3.cloudfront.net/forticlient/downloads/FortiClientVPNSetup_6.2.6.0951_x64.exe'
-$checksum64    = '4A0E4D3BB2F969FBBD61D67180604E035D89A44FC1D664B5CBA14368BB3FA671'
+$packageName = 'forticlientvpn'
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+$url64 = 'https://links.fortinet.com/forticlient/win/vpnagent'
+$checksum64 = 'D9879EC8B0EB1FC3D47C2476198CEE8C9B56CA733E89634F6FE13B405508A6F5'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = $url
   url64         = $url64
-  silentArgs    = '/QUIET /NORESTART'
   softwareName  = 'FortiClient VPN' 
-  checksum      = $checksum
   checksum64    = $checksum64
   checksumType  = 'sha256'
-  }
+}
 
 Install-ChocolateyPackage @packageArgs
